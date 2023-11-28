ab6e07f98259095d7e3a52f9cdfdc63e1c03184c
arunothsymen <141496908+arunothsymen@users.noreply.github.com>
2023-11-28 23:44:37 +0530

    Add files via upload

---
+++ symen/Lab4/assignment/MusicTeacher.java
@@ -0,0 +1,14 @@
+// File: MusicTeacher.java
+package symen.lab4.assignment;
+
+public class MusicTeacher {
+    private String name;
+
+    public MusicTeacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
